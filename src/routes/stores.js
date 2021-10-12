f17df573dea0932fbc985fbdac56c65b8c402d63
Naveen <catchspider2002@gmail.com>
2021-10-12 18:50:26 +0530

    Added Dutch translations

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 // import { onMount } from "svelte";
 
 export const themeStore = writable("light");
-export const langStore = writable("de");
-export const webStore = writable("https://de.pattern.monster");
+export const langStore = writable("nl");
+export const webStore = writable("https://nl.pattern.monster");
 
 // export let themeStore;
