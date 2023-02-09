e9bd147b4fc705f29ef9d9d8858292615462579c
Naveen CS <catchspider2002@gmail.com>
2023-02-09 08:32:22 +0530

    Added ukranian lang files

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 // import { onMount } from "svelte";
 
 export const themeStore = writable("light");
-export const langStore = writable("sv");
-export const webStore = writable("https://sv.pattern.monster");
+export const langStore = writable("uk");
+export const webStore = writable("https://uk.pattern.monster");
 
 // export let themeStore;
